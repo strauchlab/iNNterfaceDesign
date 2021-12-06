0887ca3d8aa329e6752b78e4a8063855d0b9a585
Raulia Syrlybaeva <Raulia.Syrlybaeva@uga.edu>
2021-12-06 14:07:30 -0500

    Add files via upload

---
+++ iNNterfaceDesign_scripts/1.preprocessing.py
@@ -0,0 +1,219 @@
+import os, numpy as np, sys
+from modules import functions as f, geometry_functions as fm, make_input, Class_protein as cp
+from modules import Amino_acid_dictionaries as ad
+from pyrosetta import *
+from pyrosetta.teaching import *
+from pyrosetta.rosetta.core.pose import *
+from pyrosetta.toolbox import *
+from pyrosetta.bindings.pose import *
+from pyrosetta.rosetta.core.scoring.sasa import *
+
+
+init('-ignore_unrecognized_res T -no_his_his_pairE T -no_optH F -use_input_sc T -ex1 T -ex2 T -ignore_zero_occupancy F ')
+flat = lambda x: [i for j in x for i in j]
+tolist = np.ndarray.tolist
+DSSP = pyrosetta.rosetta.protocols.moves.DsspMover()
+
+
+def get_core_amns_from_pdb(amns):
+    amns_dict = {a.amn_number: a for a in amns}
+    atoms = flat([a.atoms_no_h for a in amns])
+    atoms_xyz = [at.xyz for at in atoms]
+    neighs = fm.neighs_dict(atoms_xyz, atoms_xyz)
+    core = fm.find_core(atoms_xyz, atoms_xyz, neighs)
+    atoms_core, count = np.unique([i.amn_obj.amn_number for idx, i in enumerate(atoms) if idx in core],
+                                  return_counts=True)
+    atoms_core_list = [[amns_dict[atoms_core[i]], len(amns_dict[atoms_core[i]].atoms_no_h) - count[i]] for i in
+                       range(len(count))]
+    atoms_core_amns = [i[0] for i in atoms_core_list if i[1] == 0]
+    return atoms_core_amns
+
+def make_folders(prefix):
+    if os.path.isdir(prefix) == False:
+        os.mkdir(prefix)
+    if os.path.isdir(prefix + '/pockets') == False:
+        os.mkdir(prefix + '/pockets')
+    if os.path.isdir(prefix + '/binders') == False:
+        os.mkdir(prefix + '/binders')
+
+def get_protein(pdb, keywords):
+    protein = cp.Protein(pdb + '.pdb')
+    all_amns = flat([chain.amns for chain in protein.chains])
+    clean_pose = pose_from_pdb(pdb + '.pdb')
+    DSSP.apply(clean_pose)
+    ss = clean_pose.secstruct()
+    if len(ss) != len(all_amns):
+        print(pdb, ': number of residues in Rodetta pose and initial structure do not match')
+        sys.exit()
+    if 'p_chains' in keywords:
+        p_chains = [a.strip() for a in keywords['p_chains'].split(',')]
+        all_amns = [a for a in all_amns if  a.chain_obj.label in p_chains]
+    all_amns.sort(key=lambda x: int(x.amn_number))
+    return protein, all_amns, ss, clean_pose
+
+def get_core_amns(all_amns, keywords, clean_pose):
+    prefix = keywords['prefix']
+    if 'add_residues' in keywords:
+        core_amns = f.readjson(prefix + '/' + prefix + '_2b.json')[0]['core']
+        core_amns = [i for i in all_amns if int(i.amn_number) in core_amns]
+    elif ('get_surf_res' in keywords) and (keywords['get_surf_res'] == 'pyrosetta'):
+        sasac = np.array(rel_per_res_sc_sasa(clean_pose))
+        core = [ax+1 for ax, a in enumerate(sasac) if a <= 0.1]
+        core_amns  = [i for i in all_amns if int(i.amn_number) in core]
+    else:
+        core_amns = get_core_amns_from_pdb(all_amns)
+    return core_amns
+
+def get_prev_binders(keywords):
+    b_names = None
+    prefix = keywords['prefix']
+    if ('binders_list' in keywords) and (keywords['binders_list'] != ''):
+        b_names = f.readfile(keywords['binders_list'], 't')
+        b_names = [a.strip() for a in b_names.split(',')]
+    elif os.path.exists(prefix + '/' + prefix + '_names.json'):
+        b_names = f.readjson(prefix + '/' + prefix + '_names.json')
+    return b_names
+
+def get_anchor_res(all_amns, all_ca_atoms, surf_amns, keywords):
+    binder_xyz = None
+    if ('add_residues' in keywords) and (keywords['add_residues'] == 'True'):
+        b_names = get_prev_binders(keywords)
+        if b_names is None:
+            print('The list of binder to prolongate is not provided')
+            exit()
+        ctrl_resi = []
+        binder_xyz = []
+        for b_name in b_names:
+            if os.path.exists(prefix + '/binders_id/' + b_name[:-4] + '_id.pdb'):
+                binder = cp.Protein(prefix + '/binders_id/' + b_name[:-4] + '_id.pdb')
+                b_xyz = np.array([[x.xyz for x in a.atoms_no_h[:4]] for a in binder.chains[0].amns])
+                b_xyz1 = b_xyz[0][1]
+                b_xyz2 = b_xyz[-1][1]
+                close_amns1 = sorted([[a.amn_obj, f.distance(b_xyz1, a.xyz)] for a in all_ca_atoms], key=lambda x: x[1])[0][0]
+                close_amns2 = sorted([[a.amn_obj, f.distance(b_xyz2, a.xyz)] for a in all_ca_atoms], key=lambda x: x[1])[0][0]
+                ctrl_resi += [close_amns1.amn_number, close_amns2.amn_number]
+                binder_xyz.append([b_name, b_xyz, close_amns1.amn_number, close_amns2.amn_number])
+        ctrl_resi = [a for a in all_amns if a.amn_number in ctrl_resi]
+
+    elif 'anchor_res' in keywords:
+        anums = flat(f.get_explicit_numbers(keywords['anchor_res']))
+        ctrl_resi = [i for i in all_amns if int(i.amn_number) in anums]
+    elif 'chain' in keywords:
+        chains = [a.strip() for a in keywords['chain'].split(',')]
+        ctrl_resi = [i for i in all_amns if (i.chain_obj.label in chains) and (i in surf_amns)]
+    else:
+        ctrl_resi = surf_amns
+    return ctrl_resi, binder_xyz
+
+def get_ld_ldd(keywords, all_amns):
+    def get_ld_xyz(inp, amns, type):
+        ld = None
+        if type in inp:
+            ld_amn = flat(f.get_explicit_numbers(inp[type]))
+            ld_t = [a for a in amns if int(a.amn_number) in ld_amn]
+            if ld_t != []:
+                ld = [a.atoms_no_h[1].xyz for a in ld_t]
+        return ld
+
+    ld1 = get_ld_xyz(keywords, all_amns, 'pos_res1')
+    ld2 = get_ld_xyz(keywords, all_amns, 'pos_res2')
+    ld = [np.array(a) if a is not None else None for a in [ld1, ld2]]
+
+    ld1d = get_ld_xyz(keywords, all_amns, 'pos_res1d')
+    ld2d = get_ld_xyz(keywords, all_amns, 'pos_res2d')
+    ldd = [np.array(a) if a is not None else None for a in [ld1d, ld2d]]
+    return ld, ldd
+
+def get_pocket_amns(all_amns, all_ca_atoms, core_amns, j):
+    thr = 6
+    neigh_pp = fm.icosahedron(np.array([0, 0, 0]), 2, 3.42) + np.array(j.atoms_no_h[1].xyz)
+    neigh_pp1 = [sorted([[n, f.distance(n, m.xyz)] for m in all_ca_atoms], key=lambda x:x[1])[0] for n in neigh_pp]
+    neigh_pp = [n[0] for n in neigh_pp1 if n[1] >= thr]
+    if len(neigh_pp) == 0:
+        neigh_pp = [n[0] for n in neigh_pp1 if n[1] >= thr - 1]
+        if len(neigh_pp) == 0:
+            return
+
+    neigh_p = [sorted([[m.amn_obj, f.distance(n, m.xyz)] for n in neigh_pp], key=lambda x:x[1])[0] for m in all_ca_atoms]
+    neigh_pa = [n for n in neigh_p if (n[1] <= 11.5) and (n[0] not in core_amns) and (
+            f.distance(j.atoms_no_h[1].xyz, n[0].atoms_no_h[1].xyz) <= 20)]
+    neigh_pa.sort(key=lambda x: x[-1])
+    neigh_pa = neigh_pa[:30]
+    neigh_pa = [n[0] for n in neigh_pa]
+    neigh_ps = [a for a in all_amns if (a not in neigh_pa) and (int(a.amn_number) in flat(
+        [[int(i.amn_number) - 1, int(i.amn_number) + 1] for i in neigh_pa]))]
+    neigh_ps = [
+        sorted([[a, f.distance(a.atoms_no_h[1].xyz, n.atoms_no_h[1].xyz)] for n in neigh_pa], key=lambda x:x[1])[0] for
+        a in neigh_ps]
+    neigh_pa += [n[0] for n in neigh_ps if n[1] < 4.5]
+    neigh_pa = [sorted([[m.amn_obj, f.distance(n.atoms_no_h[1].xyz, m.xyz)] for n in neigh_pa], key=lambda x:x[1])[0]
+                for m in all_ca_atoms]
+    neigh_pa = [n for n in neigh_pa if (n[1] <= 8)]
+    neigh_pa.sort(key=lambda x: x[-1])
+    neigh_pa = neigh_pa[:48]
+    neigh_pa = [n[0] for n in neigh_pa]
+    return neigh_pa
+
+def get_features(keywords):
+    pdb = keywords['pdb']
+    prefix = keywords['prefix']
+    interf_type = keywords['interf_type']
+    interf_res = None if ('interf_res' not in keywords) else f.get_explicit_numbers(keywords['interf_res'])
+
+    make_folders(prefix)
+    protein, all_amns, ss, clean_pose = get_protein(pdb, keywords)
+    core_amns = get_core_amns(all_amns, keywords, clean_pose)
+    surf_amns = [i for i in all_amns if i not in core_amns]
+    all_atoms = flat([amn.atoms_no_h for amn in all_amns])
+    all_ca_atoms = [a for a in all_atoms if a.type == 'CA']
+    ctrl_resi, binder_xyz = get_anchor_res(all_amns, all_ca_atoms, surf_amns, keywords)
+    ld, ldd = get_ld_ldd(keywords, all_amns)
+    data = []
+    for jx, j in enumerate(ctrl_resi[:]):
+        ch_label = j.chain_obj.label
+        pocket_amns = get_pocket_amns(all_amns, all_ca_atoms, core_amns, j)
+        if (pocket_amns is None) or (j not in pocket_amns):
+            continue
+
+        set1 = [pocket_amns[a].amn_number for a in range(len(pocket_amns))]
+        if len(pocket_amns) >= 24:
+            pocket_amns.sort(key=lambda x: int(x.amn_number))
+            pocket_amns_pdb = ''.join([amn.amn_pdb for amn in pocket_amns])
+            file_name = prefix + '_' + ch_label + str(j.amn_number)
+            f.writefile(prefix + '/pockets/' + file_name + 'pocket.pdb', pocket_amns_pdb)
+            pocket_atoms = [amn.atoms_no_h for amn in pocket_amns]
+            pocket_atoms = [[ad.amn_mapping[a[0].amn_obj.type], a[0].amn_obj.amn_number, [at.xyz for at in a]] for ax, a
+                            in enumerate(pocket_atoms)]   
+            data_dir= {'core': [int(a.amn_number) for a in core_amns], 'pdb': file_name, 'interf_type': interf_type, 'ld': ld, 'ldd': ldd,
+                         'central_resi': int(j.amn_number), 'pocket_struct': pocket_atoms, 'secondary_p_all': ss,
+                        'secondary_p': [ss[int(a.amn_number) - 1] for a in pocket_amns], 'set1': set1, 'ppi': interf_res}
+            if 'add_residues' in keywords:
+                b_data = [a for a in binder_xyz if j.amn_number in a[2:]]
+                for b_d in b_data:
+                    data_dir2 = dict(data_dir)
+                    kx = 0 if j.amn_number == b_d[2] else 1
+                    data_dir2['binder_end'] = kx
+                    if kx == 0:
+                        data_dir2['binder_name'] = b_d[0][:-4]
+                        data_dir2['binder_xyz'] = b_d[1][:4].tolist()
+                    else:
+                        data_dir2['binder_name'] = b_d[0][:-4]
+                        data_dir2['binder_xyz'] = b_d[1][-4:].tolist()
+                    data.append(data_dir2) 
+            else: 
+                data.append(data_dir) 
+    return data
+
+
+input_file = sys.argv[1]
+keywords = f.keywords(input_file)
+prefix = keywords['prefix']
+features = get_features(keywords)
+if features != []:
+    data1, data2 = make_input.make_data(features)
+    f.writejson(prefix + '/' + prefix + '_b.json', data1)
+    f.writejson(prefix + '/' + prefix + '_2b.json', data2)
+else:
+    print('Operation has completed with error')
+
+
---
+++ iNNterfaceDesign_scripts/2.binders.py
@@ -0,0 +1,195 @@
+import sys,numpy as np
+from modules import functions as f, transform_coords as tc
+from tensorflow.keras.models import load_model
+
+sst_dict = {'H': 0, 'E':1, 'L': 2}
+dir_models = '/home/raulia/binders_nn/modules/models/'
+def arr_ranges(data):
+    data = [f.make_ranges(data[k], 50) for k in range(len(data))]
+    data = [[a[k] for a in data] for k in range(len(data[0]))]
+    return data
+
+def data_for_orient_func(k):
+    inp3 = np.zeros((k, 2))
+    inp3[:, 0] = 1
+    return [inp3]
+
+def data_for_binder_func(k, sstr):
+    inp3 = np.zeros((k, 2))
+    inp3[:, 0] = 1
+    temp = np.zeros((k, 6, 3))
+    for idx in range(k):
+        for jdx in range(2, 4):
+            temp[idx, jdx, int(sstr[idx, jdx-2])] = 1
+    return [temp, inp3]
+
+def orient_predictions_func(data, keywords, ld, ldd, model):
+    test_ch = arr_ranges(data)
+    y_pred = [model.predict(i, verbose=0) for i in test_ch]
+    y_pred = np.vstack(y_pred)
+    pred_order = np.fliplr(np.argsort(y_pred, axis=1))
+    pred_order = [[[i,y_pred[jx, i]]  for i in j] for jx, j in enumerate(pred_order)]
+    pred_order = [[[x[0] // 6, x[0] % 6, x[1]] for x in j] for j in pred_order]
+    pos_thr = 0.01 if 'pos_thr' not in keywords else float(keywords['pos_thr'])
+    pred_order = [[x for x in a if x[2] > pos_thr] for a in pred_order]
+   
+    if ld[0] is not None:
+        pred_order = [[x for x in a if x[0] in ld[0]] for a in pred_order]
+    if ld[1] is not None:
+        pred_order = [[x for x in a if x[1] in ld[1]] for a in pred_order]
+    #print(pred_order)
+    if (ld[0] is None) and (ld[1] is None):
+        #print(ld, ldd)
+        if ldd[0] is not None:
+            pred_order = [[x for x in a if x[0] not in ldd[0]] for a in pred_order]
+        if ldd[1] is not None:
+            pred_order = [[x for x in a if x[1] in ldd[1]] for a in pred_order]
+    max_pos = 1 if 'max_pos' not in keywords else int(keywords['max_pos'])
+    pred_order = [i[:max_pos] for i in pred_order]
+    print(pred_order)
+    return pred_order
+
+def sstr_predictions_func(data, keywords, model):
+    #print([i.shape for i in data])
+    test_ch = arr_ranges(data)
+    y_pred = [model.predict(i, verbose=0) for i in test_ch]
+    y_pred = np.vstack(y_pred)
+    pred_order = np.fliplr(np.argsort(y_pred, axis=1))
+    pred_order = [[[i,y_pred[jx, i]]  for i in j if y_pred[jx, i]] for jx, j in enumerate(pred_order)]
+    pred_order = [[[x[0] // 3, x[0] % 3, x[1]] for x in j] for j in pred_order]
+    sst_thr = 0.01 if 'sst_thr' not in keywords else float(keywords['sst_thr'])
+    pred_order = [[x for x in a if x[2] > sst_thr] for a in pred_order]
+    if ('sst_type' in keywords):
+        sst_type = [sst_dict[a.strip()] for a in keywords['sst_type'].split(',')]
+        pred_order = [[x for x in a if len([j for j in x[:2] if j in sst_type]) == 2] for a in pred_order]
+    #else
+   
+    max_sst = 1 if 'max_sst' not in keywords else int(keywords['max_sst'])
+    pred_order = [i[:max_sst] for i in pred_order]
+    return pred_order
+
+def binders_predictions_func(data, o, s, model):
+    o, s = [str(np.round(int(i[-1]*100))) for i in o], [str(np.round(int(i[-1]*100))) for i in s]
+    test_ch = arr_ranges(data)
+    y_pred = [model.predict(i, verbose=0) for i in test_ch]
+    y_pred = np.vstack(y_pred)
+    return [y_pred, o, s]
+
+def make_binders(data, data2, keywords):
+    model_pos = load_model(dir_models + 'Orn.hdf5')
+    model_sst = load_model(dir_models + 'SecS.hdf5')
+    model_bb = load_model(dir_models + 'PepBB.hdf5')
+    swap = 'False'
+    if ('swap_pose' in keywords) and (keywords['swap_pose'] == 'True'):
+        swap = 'True'
+
+    pdb_data_all = []
+    binder_files = []
+    data = f.readjson(data)
+    data2 = f.readjson(data2)
+    p_geom, p_sec, p_amn, p_mask, name = [np.array(data[x]) for x in ['p_xyz1', 'sec1', 'p_amn1', 'mask', 'name']]
+    data_for_orient_n = data_for_orient_func(len(name))
+    orients = orient_predictions_func([p_geom, p_sec, p_amn, p_mask] + data_for_orient_n, keywords, data2[0]['ld'], data2[0]['ldd'], model_pos)
+    num_orient = max([len(i) for i in orients])
+    for ornt in range(num_orient):
+        idx = [idx for idx in range(len(orients)) if len(orients[idx]) > ornt]
+        p_geom_idx, p_sec_idx, p_amn_idx, p_mask_idx, name_idx = [a[idx] for a in [p_geom, p_sec, p_amn, p_mask, name]]
+        ornt_idx = np.array([o[ornt] for ox, o in enumerate(orients) if ox in idx])[:, :2]
+        data_for_sstr_n = data_for_orient_func(len(name_idx))
+        sstrs = sstr_predictions_func([p_geom_idx, p_sec_idx, p_amn_idx, p_mask_idx, ornt_idx] + data_for_sstr_n, keywords, model_sst)    
+        num_sstr = max([len(i) for i in sstrs])
+        for sstr in range(num_sstr):
+            jdx = [idx for idx in range(len(sstrs)) if len(sstrs[idx]) > sstr]
+            p_geom_jdx, p_sec_jdx, p_amn_jdx, p_mask_jdx, name_jdx = [a[jdx] for a in [p_geom_idx, p_sec_idx, p_amn_idx, p_mask_idx, name_idx]]
+            p_ld_jdx = ornt_idx[jdx]
+            sstr_jdx = np.array([s[sstr] for sx, s in enumerate(sstrs) if sx in jdx])[:, :2]
+            data_for_binder_n = data_for_binder_func(len(name_jdx), sstr_jdx)
+            binders_data = binders_predictions_func([p_geom_jdx, p_sec_jdx, p_amn_jdx, p_mask_jdx, p_ld_jdx] + data_for_binder_n, ornt_idx, sstr_jdx, model_bb)
+            pdb_data = tc.calculate_rmsd(binders_data, data2, name_jdx, [str(ornt), str(sstr)], sstr_jdx.tolist(), keywords['prefix'], swap)
+            pdb_data_all += pdb_data
+            #print(pdb_data_all)
+    return pdb_data_all
+
+def make_binders_E(data, data2, keywords, e, c_names = None):
+    nbem = 1
+    swap = 'False'
+    if ('swap_pose' in keywords) and (keywords['swap_pose'] == 'True'):
+        swap = 'True'
+
+    if ('num_pepbbe_m' in keywords) and (int(keywords['num_pepbbe_m']) in [1, 3]):
+        nbem = int(keywords['num_pepbbe_m'])
+    if e == 1:
+        model_pos = load_model(dir_models + 'Orn_C.hdf5')
+        model_sst = load_model(dir_models + 'SecS_C.hdf5')
+        model_bb = [load_model(dir_models + 'PepBB_C_' + str(k) + '.hdf5') for k in range(1,nbem+1)]
+
+
+    elif (e == 0) or (e==10):
+        model_pos = load_model(dir_models + 'Orn_N.hdf5')
+        model_sst = load_model(dir_models + 'SecS_N.hdf5')
+        model_bb = [load_model(dir_models + 'PepBB_N' + str(k) + '.hdf5') for k in range(1,nbem+1)]
+
+
+
+    p_geom, p_sec, p_amn, p_mask, name, name_b, l_geom = data
+    
+    pdb_data_all = []
+    binder_files = []
+
+    data2 = f.readjson(data2)
+    data_for_orient_n = data_for_orient_func(len(name))
+    orients = orient_predictions_func([p_geom, p_sec, p_amn, p_mask] + data_for_orient_n + [l_geom], keywords, data2[0]['ld'], data2[0]['ldd'], model_pos)
+    num_orient = max([len(i) for i in orients])
+    for ornt in range(num_orient):
+        idx = [idx for idx in range(len(orients)) if len(orients[idx]) > ornt]
+        p_geom_idx, p_sec_idx, p_amn_idx, p_mask_idx, name_idx, name_b_idx, l_geom_idx = [a[idx] for a in [p_geom, p_sec, p_amn, p_mask, name, name_b, l_geom]]
+        ornt_idx = np.array([o[ornt] for ox, o in enumerate(orients) if ox in idx])[:, :2]
+        data_for_sstr_n = data_for_orient_func(len(name_idx))
+        sstrs = sstr_predictions_func([p_geom_idx, p_sec_idx, p_amn_idx, p_mask_idx, ornt_idx] + data_for_sstr_n + [l_geom_idx], keywords, model_sst)    
+        num_sstr = max([len(i) for i in sstrs]) 
+        for sstr in range(num_sstr):
+            for mm in range(len(model_bb)):
+                jdx = [idx for idx in range(len(sstrs)) if len(sstrs[idx]) > sstr]
+                p_geom_jdx, p_sec_jdx, p_amn_jdx, p_mask_jdx, name_jdx,  name_b_jdx, l_geom_jdx = [a[jdx] for a in [p_geom_idx, p_sec_idx, p_amn_idx, p_mask_idx, name_idx, name_b_idx, l_geom_idx]]
+                p_ld_jdx = ornt_idx[jdx]
+                sstr_jdx = np.array([s[sstr] for sx, s in enumerate(sstrs) if sx in jdx])[:, :2]
+                data_for_binder_n = data_for_binder_func(len(name_jdx), sstr_jdx)
+                binders_data = binders_predictions_func([p_geom_jdx, p_sec_jdx, p_amn_jdx, p_mask_jdx, p_ld_jdx] + data_for_binder_n + [l_geom_jdx[:,:,:,[0,1,2,4]]], ornt_idx, sstr_jdx, model_bb[mm])
+                pdb_data = tc.calculate_rmsde(binders_data, data2, name_jdx, name_b_jdx, [str(ornt), str(sstr), str(mm)], sstr_jdx.tolist(), keywords['prefix'], e, swap, c_names)
+                pdb_data_all += pdb_data
+    return pdb_data_all
+
+def make_binders_end(data, data2, keywords):
+    def get_e_data(data, e):
+        idx = np.where(np.array(data['b_end']) == e)[0]
+        data = [np.array([a for ax, a in enumerate(data[x]) if ax in idx]) for x in data if x != 'b_end']
+        return data
+
+    data = f.readjson(data)
+    pdb_names_all = []
+    if 'binder_end' in keywords:
+        if keywords['binder_end'] == 'C':
+            data_C  = get_e_data(data, 1)
+            pdb_names = make_binders_E(data_C, data2, keywords, 1)   
+        elif keywords['binder_end'] == 'N':
+            data_N  = get_e_data(data, 0)
+            pdb_names = make_binders_E(data_N, data2, keywords, 0) 
+    else:
+        data_C  = get_e_data(data, 1)
+        pdb_names = make_binders_E(data_C, data2, keywords, 1)         
+        data_N  = get_e_data(data, 0)
+        pdb_names = make_binders_E(data_N, data2, keywords, 10, pdb_names)   
+    pdb_names_all += pdb_names
+    return pdb_names
+
+input_file = sys.argv[1]
+keywords = f.keywords(input_file)
+prefix = keywords['prefix']
+data = prefix + '/' + prefix  + '_b.json'
+data2 = prefix + '/' + prefix + '_2b.json'
+if 'add_residues' in keywords:
+    pdb_names = make_binders_end(data, data2, keywords)
+else:
+    pdb_names = make_binders(data, data2, keywords)
+print(pdb_names)
+f.writejson(prefix + '/' + prefix + '_names.json', pdb_names)
